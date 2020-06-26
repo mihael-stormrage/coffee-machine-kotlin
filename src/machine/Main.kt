6f90af032d25844d9244b39ccb527c74245d4b0a
Mihael Stormrage <mihan.msn@hotmail.com>
2020-06-26 07:15:13 +0700

    Stage #3: Enough coffee for everyone

--- src/machine/Main.kt
+++ src/machine/Main.kt
@@ -5,13 +5,19 @@ import java.util.*
 val scanner = Scanner(System.`in`)
 
 fun main() {
+    print("Write how many ml of water the coffee machine has: ")
+    val water = scanner.nextInt()
+    print("Write how many ml of milk the coffee machine has: ")
+    val milk = scanner.nextInt()
+    print("Write how many grams of coffee beans the coffee machine has: ")
+    val coffee = scanner.nextInt()
     print("Write how many cups of coffee you will need: ")
     val cups = scanner.nextInt()
-    val water = cups * 200
-    val milk = cups * 50
-    val coffee = cups * 15
-    println("""For $cups of coffee you will need:
-        |$water ml of water
-        |$milk ml of milk
-        |$coffee g of coffee beans""".trimMargin())
+
+    val can = minOf(water / 200, milk / 50, coffee / 15)
+    println(when {
+        can > cups -> "Yes, I can make that amount of coffee (and even ${can - cups} more than that)"
+        can < cups -> "No, I can make only $can cups of coffee"
+        else -> "Yes, I can make that amount of coffee"
+    })
 }
