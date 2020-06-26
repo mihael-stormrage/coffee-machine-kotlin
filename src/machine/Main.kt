ecefa5a52895f9b8b548196201f4f07e875b0f52
Mihael Stormrage <mihan.msn@hotmail.com>
2020-06-26 08:17:08 +0700

    OOP: Implement class with functions

--- src/machine/Main.kt
+++ src/machine/Main.kt
@@ -5,11 +5,39 @@ import java.util.*
 val scanner = Scanner(System.`in`)
 
 fun main() {
-    var water = 400
-    var milk = 540
-    var coffee = 120
-    var dispcups = 9
-    var money = 550
+    val coffeeMachine = CoffeeMachine()
+
+    print("${coffeeMachine.state()}\n\nWrite action (buy, fill, take): ")
+    when (scanner.next()) {
+        "buy" -> {
+            print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ")
+            coffeeMachine.buy(scanner.nextInt())
+        }
+        "fill" -> {
+            print("Write how many ml of water do you want to add: ")
+            val water = scanner.nextInt()
+            print("Write how many ml of milk do you want to add: ")
+            val milk = scanner.nextInt()
+            print("Write how many grams of coffee beans do you want to add: ")
+            val coffee = scanner.nextInt()
+            print("Write how many disposable cups of coffee do you want to add: ")
+            val dispcups = scanner.nextInt()
+            coffeeMachine.fill(water, milk, coffee, dispcups)
+        }
+        "take" -> {
+            println("I gave you $${coffeeMachine.take()}")
+        }
+    }
+    println("\n${coffeeMachine.state()}")
+}
+
+class CoffeeMachine(
+        private var water: Int = 400,
+        private var milk: Int = 540,
+        private var coffee: Int = 120,
+        private var dispcups: Int = 9,
+        private var money: Int = 550) {
+
     fun state() = """The coffee machine has:
         |$water of water
         |$milk of milk
@@ -17,31 +45,25 @@ fun main() {
         |$dispcups of disposable cups
         |$money of money""".trimMargin()
 
-    print("${state()}\n\nWrite action (buy, fill, take): ")
-    when (scanner.next()) {
-        "buy" -> {
-            print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ")
-            when (scanner.nextInt()) {
-                1 -> { water -= 250; coffee -= 16; money += 4 }
-                2 -> { water -= 350; milk -= 75; coffee -= 20; money += 7 }
-                3 -> { water -= 200; milk -= 100; coffee -= 12; money += 6 }
-            }
-            dispcups -= 1
+    fun buy(variety: Int) {
+        when (variety) {
+            1 -> { water -= 250; coffee -= 16; money += 4 }
+            2 -> { water -= 350; milk -= 75; coffee -= 20; money += 7 }
+            3 -> { water -= 200; milk -= 100; coffee -= 12; money += 6 }
         }
-        "fill" -> {
-            print("Write how many ml of water do you want to add: ")
-            water += scanner.nextInt()
-            print("Write how many ml of milk do you want to add: ")
-            milk += scanner.nextInt()
-            print("Write how many grams of coffee beans do you want to add: ")
-            coffee += scanner.nextInt()
-            print("Write how many disposable cups of coffee do you want to add: ")
-            dispcups += scanner.nextInt()
-        }
-        "take" -> {
-            print("I gave you $$money")
-            money = 0
-        }
+        dispcups -= 1
     }
-    println("\n${state()}")
+
+    fun fill(_water: Int, _milk: Int, _coffee: Int, _dispcups: Int) {
+        water += _water
+        milk += _milk
+        coffee += _coffee
+        dispcups += _dispcups
+    }
+
+    fun take(): Int {
+        val buffer = money
+        money = 0
+        return buffer
+    }
 }
