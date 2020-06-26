d9576f304709ded3f5e2213098e2a1f70c6eef36
Mihael Stormrage <mihan.msn@hotmail.com>
2020-06-26 07:58:57 +0700

    Stage #4: Action!

--- src/machine/Main.kt
+++ src/machine/Main.kt
@@ -5,19 +5,43 @@ import java.util.*
 val scanner = Scanner(System.`in`)
 
 fun main() {
-    print("Write how many ml of water the coffee machine has: ")
-    val water = scanner.nextInt()
-    print("Write how many ml of milk the coffee machine has: ")
-    val milk = scanner.nextInt()
-    print("Write how many grams of coffee beans the coffee machine has: ")
-    val coffee = scanner.nextInt()
-    print("Write how many cups of coffee you will need: ")
-    val cups = scanner.nextInt()
+    var water = 400
+    var milk = 540
+    var coffee = 120
+    var dispcups = 9
+    var money = 550
+    fun state() = """The coffee machine has:
+        |$water of water
+        |$milk of milk
+        |$coffee of coffee beans
+        |$dispcups of disposable cups
+        |$money of money""".trimMargin()
 
-    val can = minOf(water / 200, milk / 50, coffee / 15)
-    println(when {
-        can > cups -> "Yes, I can make that amount of coffee (and even ${can - cups} more than that)"
-        can < cups -> "No, I can make only $can cups of coffee"
-        else -> "Yes, I can make that amount of coffee"
-    })
+    print("${state()}\n\nWrite action (buy, fill, take): ")
+    when (scanner.next()) {
+        "buy" -> {
+            print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ")
+            when (scanner.nextInt()) {
+                1 -> { water -= 250; coffee -= 16; money += 4 }
+                2 -> { water -= 350; milk -= 75; coffee -= 20; money += 7 }
+                3 -> { water -= 200; milk -= 100; coffee -= 12; money += 6 }
+            }
+            dispcups -= 1
+        }
+        "fill" -> {
+            print("Write how many ml of water do you want to add: ")
+            water += scanner.nextInt()
+            print("Write how many ml of milk do you want to add: ")
+            milk += scanner.nextInt()
+            print("Write how many grams of coffee beans do you want to add: ")
+            coffee += scanner.nextInt()
+            print("Write how many disposable cups of coffee do you want to add: ")
+            dispcups += scanner.nextInt()
+        }
+        "take" -> {
+            print("I gave you $$money")
+            money = 0
+        }
+    }
+    println("\n${state()}")
 }
